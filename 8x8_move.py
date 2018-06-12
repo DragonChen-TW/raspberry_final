75619b6700501e7c80dd2db83f1c58e5e635f8c1
Jackson Chen <teacher144123@gmail.com>
2018-06-12 19:51:24 +0800

    split into register and 8x8

--- 8x8_move.py
+++ 8x8_move.py
@@ -2,15 +2,15 @@ import json, time
 from pprint import pprint
 
 def show8x8(image, sec):
-    for _ in range(int(1000 * sec)):
-        for i in range(8):
+    for _ in range(int(100 * sec)):
+        for i in range(1):
             for j in range(8):
                 if image[i][j]:
                     gpio.output(row_led[j], gpio.HIGH)
                 else:
                     gpio.output(row_led[j], gpio.LOW)
             gpio.output(col_led[i], gpio.LOW)
-            time.sleep(0.001)
+            time.sleep(0.01)
             gpio.output(col_led[i], gpio.HIGH)
 
 def print8x8(words, step=1, width=8, delay=1):
@@ -18,11 +18,11 @@ def print8x8(words, step=1, width=8, delay=1):
 
     for i in range(0, len(graph) - width + 1, step):
         graph_slice = graph[i:i + width]
-
         print("\n".join(graph_slice))
         print()
 
         # print to 8x8
+        show8x8(graph_slice, sec=delay)
 
         time.sleep(delay)
 
---
+++ register.py
@@ -0,0 +1,47 @@
+import RPi.GPIO as gpio
+
+def setup():
+    # global variables
+    global DS, SHCP, STCP
+    # global CLK, MOSI, CE
+    DS = 17
+    SHCP = 27
+    STCP = 22
+
+    gpio.setmode(gpio.BCM)
+    gpio.setup(DS, gpio.OUT)
+    gpio.setup(SHCP, gpio.OUT)
+    gpio.setup(STCP, gpio.OUT)
+
+def makeTick(gpio_num):
+    gpio.output(gpio_num, gpio.HIGH)
+    gpio.output(gpio_num, gpio.LOW)
+
+def shift(shift_data):
+    for i in shift_data:
+        gpio.output(SHCP, gpio.LOW)
+        gpio.output(DS, int(i))
+        gpio.output(SHCP, gpio.HIGH)
+        gpio.output(DS, 0)
+
+
+def hc_out(data):
+    for each in data:
+        gpio.output(STCP, gpio.LOW)
+        shift(each)
+        gpio.output(STCP, gpio.HIGH)
+
+        # gpio.output(STCP, gpio.LOW)
+        # shift('00000000')
+        # gpio.output(STCP, gpio.HIGH)
+
+if __name__ =="__main__":
+    try:
+        setup()
+
+        data = ['00001111', '11000000', '11111111', '00000000']
+        data *= 3
+
+        hc_out(data)
+    finally:
+        gpio.cleanup()
