e1b35a65ef5f36797297cfb92c327fe7ae208831
Jackson Chen <teacher144123@gmail.com>
2018-06-12 20:10:01 +0800

    add show8x8 and setup

--- 8x8_move.py
+++ 8x8_move.py
@@ -1,17 +1,32 @@
 import json, time
 from pprint import pprint
 
+def setup():
+    # global variables
+    global DS, SHCP, STCP
+    # global CLK, MOSI, CE
+    # gpio left -----OOO--------000-
+    DS =    [17, 13]
+    SHCP =  [27, 29]
+    STCP =  [22, 26]
+
+    gpio.setmode(gpio.BCM)
+    for g in DS + SHCP + STCP:
+        gpio.setup(g, gpio.OUT)
+
 def show8x8(image, sec):
     for _ in range(int(100 * sec)):
         for i in range(1):
             for j in range(8):
                 if image[i][j]:
-                    gpio.output(row_led[j], gpio.HIGH)
+                    # gpio.output(row_led[j], gpio.HIGH)
+                    print('1')
                 else:
-                    gpio.output(row_led[j], gpio.LOW)
-            gpio.output(col_led[i], gpio.LOW)
-            time.sleep(0.01)
-            gpio.output(col_led[i], gpio.HIGH)
+                    # gpio.output(row_led[j], gpio.LOW)
+                    print('0')
+            # gpio.output(col_led[i], gpio.LOW)
+            # time.sleep(0.01)
+            # gpio.output(col_led[i], gpio.HIGH)
 
 def print8x8(words, step=1, width=8, delay=1):
     graph = makeGraph(words)
