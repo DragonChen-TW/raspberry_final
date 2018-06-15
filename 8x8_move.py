69f3e50e600a16b42e4b18918f70777d4b611a35
Jackson Chen <teacher144123@gmail.com>
2018-06-15 21:21:47 +0800

    change to class

--- 8x8_move.py
+++ 8x8_move.py
@@ -3,65 +3,61 @@ import json, time, random
 
 from register import Register
 
-def setup():
-    # global variables
-    global DS, SHCP, STCP
-    global register
-    # global CLK, MOSI, CE
-    # gpio left -----OOO--------000-
-    DS =    [17, 13]
-    SHCP =  [22, 26]
-    STCP =  [27, 19]
+class LEDMatrix:
+    def __init__(self, num_layer=20):
+        self.DS =    [17, 13]
+        self.SHCP =  [22, 26]
+        self.STCP =  [27, 19]
+        self.register = Register(DS, SHCP, STCP)
 
-    # gpio setup
-    gpio.setmode(gpio.BCM)
-    for g in DS + SHCP + STCP:
-        gpio.setup(g, gpio.OUT)
+        # gpio setup
+        gpio.setmode(gpio.BCM)
+        for g in DS + SHCP + STCP:
+            gpio.setup(g, gpio.OUT)
 
-    # register and init LED
-    register = Register(DS, SHCP, STCP)
-    register.shift(1, '11111111')
+        self.now_layer = 0
 
-def show8x8(graph, sec):
-    global register
-    temp = [
-        '11111110',
-        '11111101',
-        '11111011',
-        '11110111',
-        '11101111',
-        '11011111',
-        '10111111',
-        '01111111',
-    ]
-    for _ in range(int(100 * sec)):
-        for i in range(8):
-            register.shift(0, graph[i])
+        self.graph = self.makeGraph(num_layer)
 
-            register.shift(1, temp[i])
-            time.sleep(0.001)
-            register.shift(1, '11111111')
+    def show8x8(self, sec):
+        temp = [
+            '11111110',
+            '11111101',
+            '11111011',
+            '11110111',
+            '11101111',
+            '11011111',
+            '10111111',
+            '01111111',
+        ]
+        for _ in range(int(100 * sec)):
+            for i in range(8):
+                register.shift(0, graph[i])
 
-def print8x8(graph, step=1, width=8, delay=1):
-    for i in range(0, len(graph) - width + 1, step):
-        graph_slice = graph[i:i + width]
-        print("\n".join(graph_slice))
-        print()
+                register.shift(1, temp[i])
+                time.sleep(0.001)
+                register.shift(1, '11111111')
 
-        # print to 8x8
-        show8x8(graph_slice, sec=delay)
+    def printOut(self, step=1, width=8, delay=1):
+        for i in range(0, len(self.graph) - width + 1, step):
+            graph_slice = self.graph[i:i + width]
+            print("\n".join(graph_slice))
+            print()
 
-def makeGraph(num_layer):
-    with open('data/layer.json') as json_f:
-        data = json.loads(json_f.read())
-    keys = list(data.keys())
+            # print to 8x8
+            show8x8(graph_slice, sec=delay)
 
-    graph = []
-    for i in range(num_layer):
-        r_int = random.randint(0, len(keys) - 1)
-        graph += data[keys[r_int]]
+    def makeGraph(self, num_layer):
+        with open('data/layer.json') as json_f:
+            data = json.loads(json_f.read())
+        keys = list(data.keys())
 
-    return graph
+        graph = []
+        for i in range(num_layer):
+            r_int = random.randint(0, len(keys) - 1)
+            graph += data[keys[r_int]]
+
+        return graph
 
 def makeWords(words):
     with open('data/hello.json') as json_f:
@@ -75,12 +71,8 @@ def makeWords(words):
 
 if __name__ == '__main__':
     try:
-        setup()
+        matrix = LEDMatrix()
 
-        # words = "hello"
-        # graph = makeWords(words)
-        graph = makeGraph(20)
-        print(graph)
-        print8x8(graph, delay=0.5)
+        matrix.printOut(delay=0.5)
     finally:
         gpio.cleanup()
