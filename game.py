aaeb3ba7941f4ba3fffe2efe4b455eafba08925f
Jackson Chen <teacher144123@gmail.com>
2018-06-15 21:39:54 +0800

    add threading

---
+++ game.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as gpio
+from threading import Thread
+
+from register import Register
+
+if __name__ == '__main__':
+    matrix = LEDMatrix()
+
+    th = Thread(matrix.startPrint())
+    th.start()
+
+    # btn
+    gpio.setmode(gpio.BCM)
+    LED.setup(12, 'in')
+
+    while True:
+        if not gpio.input(12):
+            print('press')
+            matrix.now_layer += 1
+
+        time.sleep(0.5)
