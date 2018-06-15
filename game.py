f37a6c26876ac264045d8d644858b79f40edf39b
Jackson Chen <teacher144123@gmail.com>
2018-06-15 21:44:54 +0800

    add try finnally btn init

--- game.py
+++ game.py
@@ -4,20 +4,23 @@ from threading import Thread
 from matrix import LEDMatrix
 
 if __name__ == '__main__':
-    matrix = LEDMatrix()
+    try:
+        matrix = LEDMatrix()
 
-    th = Thread(matrix.startPrint())
-    th.start()
+        th = Thread(matrix.startPrint())
+        th.start()
 
-    # btn
-    gpio.setmode(gpio.BCM)
+        # btn
+        gpio.setmode(gpio.BCM)
 
-    btn_gpio = 21
-    gpio.setup(btn_gpio, gpio.IN)
+        btn_gpio = 21
+        gpio.setup(btn_gpio, gpio.IN)
 
-    while True:
-        if not gpio.input(btn_gpio):
-            print('press')
-            matrix.now_layer += 1
+        while True:
+            if not gpio.input(btn_gpio):
+                print('press')
+                matrix.now_layer += 1
 
-        time.sleep(0.5)
+            time.sleep(0.5)
+    finally:
+        gpio.cleanup()
