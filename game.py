b0ba366b4ff08203a43af0e8f24648342848d823
beezzack <cliff860429@gmail.com>
2018-06-17 23:21:50 +0800

    mutibtn

--- game.py
+++ game.py
@@ -4,6 +4,12 @@ from threading import Thread
 
 from matrix import LEDMatrix
 
+
+def comparelayer(btn_map):
+    print(matrix.maps[matrix.now_layer])
+    if btn_map == matrix.maps[matrix.now_layer]:
+        matrix.now_layer += 1
+
 if __name__ == '__main__':
     try:
         matrix = LEDMatrix(100)
@@ -11,17 +17,37 @@ if __name__ == '__main__':
         matrix.startPrint()
 
         gpio.setmode(gpio.BCM)
-        btn_gpio = 21
-        gpio.setup(btn_gpio, gpio.IN)
+        btn_gpio_0001 = 21
+        btn_gpio_0010 = 16
+        btn_gpio_0100 = 7
+        btn_gpio_1000 = 23
+        gpio.setup(btn_gpio_0001, gpio.IN)
+        gpio.setup(btn_gpio_0010, gpio.IN)
+        gpio.setup(btn_gpio_0100, gpio.IN)
+        gpio.setup(btn_gpio_1000, gpio.IN)
 
         while True:
-            if not gpio.input(btn_gpio):
-                while not gpio.input(btn_gpio):
+            if not gpio.input(btn_gpio_0001):
+                while not gpio.input(btn_gpio_0001):
                     time.sleep(0.1)
-
                 # print('press')
-                print(matrix.maps[matrix.now_layer])
-                matrix.now_layer += 1
+                print('btn_gpio_0001')
+                comparelayer('0001')
+            elif not gpio.input(btn_gpio_0010):
+                while not gpio.input(btn_gpio_0010):
+                    time.sleep(0.1)
+                print('btn_gpio_0010')
+                comparelayer('0010')
+            elif not gpio.input(btn_gpio_0100):
+                while not gpio.input(btn_gpio_0100):
+                    time.sleep(0.1)
+                print('btn_gpio_0100')
+                comparelayer('0100')
+            elif not gpio.input(btn_gpio_1000):
+                while not gpio.input((btn_gpio_1000):
+                    time.sleep(0.1)
+                print('btn_gpio_1000')
+                comparelayer('1000')
 
             time.sleep(0.1)
     finally:
