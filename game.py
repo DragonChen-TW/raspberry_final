60511586cd2ddfd6e16213a0053f48c846590cb2
Jackson Chen <teacher144123@gmail.com>
2018-06-21 17:22:32 +0800

    make not layer

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ def compare_layer(layer, btn_inputs):
     pass
 
 def get_layer(name):
-    name = [int(n) for n in name]
+    name = [not int(n) for n in name]
     return name
 
 if __name__ == '__main__':
