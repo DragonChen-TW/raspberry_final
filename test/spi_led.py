0e8c2b453a191673ee04122116f85d0298664485
Jackson Chen <teacher144123@gmail.com>
2018-06-26 19:00:15 +0800

    try setting

--- test/spi_led.py
+++ test/spi_led.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
         register.put((i + 1) << 8, 12)
 
     ### Disable code B decode mode on all digits
-    register.put(int("100100000000",2), 16)
-    register.put(int("001000100111",2),12)
-    time.sleep(1)
+    # register.put(int("100100000000",2), 16)
+    # register.put(int("001000100111",2),12)
+    # time.sleep(1)
 
     ### Set intensity low
     register.put(int("101000000000",2), 16)
@@ -22,5 +22,15 @@ if __name__ == '__main__':
     register.put(int("101100000111",2), 16)
     register.put(int("001000100111",2),12)
     time.sleep(1)
+
+    ### Disable test mode
+    register.put(int("111100000000",2), 16)
+    register.put(int("001000100111",2),12)
+    time.sleep(1)
+
+    ### Disable shutdown mode
+    # register.put(int("110000000001",2), 16)
+    # register.put(int("001000100111",2),12)
+    # time.sleep(1)
 
     input("Press any key to shut down.")
