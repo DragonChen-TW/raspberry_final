6cb93cac72f192935389ea9c87d296563d5387a9
Jackson Chen <teacher144123@gmail.com>
2018-06-26 19:31:23 +0800

    try print heart

--- test/spi_led.py
+++ test/spi_led.py
@@ -12,6 +12,21 @@ if __name__ == '__main__':
     register.put(int("101100000111",2),12)
     register.put(int("110000000001",2),12)
 
-    register.put(int('010011100111', 2), 12)
+    # register.put(int('010011100111', 2), 12)
+
+    heart = [
+        '00000000',
+        '01100110',
+        '11111111',
+        '11111111',
+        '01111110',
+        '00111100',
+        '00011000',
+        '00000000',
+    ]
+
+    for i in range(8):
+        register.put((i + 1) << 8 + int(heart[i], 2), 12)
+
 
     input("Press any key to shut down.")
