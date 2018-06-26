c26e828687f79b893799ec601f3c72f426fffc36
Jackson Chen <teacher144123@gmail.com>
2018-06-26 18:59:00 +0800

    try setting

--- test/spi_led.py
+++ test/spi_led.py
@@ -8,11 +8,6 @@ if __name__ == '__main__':
     for i in range(8):
         register.put((i + 1) << 8, 12)
 
-    ### Disable code B decode mode on all digits
-    register.put(int("100100000000",2), 16)
-    register.put(int("001000100111",2),12)
-    time.sleep(1)
-
     ### Set intensity low
     register.put(int("101000000000",2), 16)
     register.put(int("001000100111",2),12)
@@ -20,16 +15,6 @@ if __name__ == '__main__':
 
     ### Enable all digits in scan-limit register
     register.put(int("101100000111",2), 16)
-    register.put(int("001000100111",2),12)
-    time.sleep(1)
-
-    ### Disable test mode
-    register.put(int("111100000000",2), 16)
-    register.put(int("001000100111",2),12)
-    time.sleep(1)
-
-    ### Disable shutdown mode
-    register.put(int("110000000001",2), 16)
     register.put(int("001000100111",2),12)
     time.sleep(1)
 
