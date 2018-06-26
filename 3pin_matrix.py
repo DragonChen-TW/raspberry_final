51370e608b0c8ee0c81dbc31a899691cb6a3b3e6
Jackson Chen <teacher144123@gmail.com>
2018-06-26 18:31:18 +0800

    add spi le

--- 3pin_matrix.py
+++ 3pin_matrix.py
@@ -2,25 +2,27 @@ import RPi.GPIO as gpio
 import json, time, random
 from threading import Thread
 
-class Register:
-    def __init__(self, DS, SHCP, STCP):
-        self.DS = DS
-        self.SHCP = SHCP
-        self.STCP = STCP
+import spi
 
-        gpio.setmode(gpio.BCM)
-        for g in self.DS + self.SHCP + self.STCP:
-            gpio.setup(g, gpio.OUT)
-
-    def makeTick(self, gpio_num):
-        gpio.output(gpio_num, gpio.HIGH)
-        gpio.output(gpio_num, gpio.LOW)
-
-    def shift(self, i, shift_data):
-        for d in shift_data:
-            gpio.output(self.DS[i], int(d))
-            self.makeTick(self.SHCP[i])
-        self.makeTick(self.STCP[i])
+# class Register:
+#     def __init__(self, DS, SHCP, STCP):
+#         self.DS = DS
+#         self.SHCP = SHCP
+#         self.STCP = STCP
+#
+#         gpio.setmode(gpio.BCM)
+#         for g in self.DS + self.SHCP + self.STCP:
+#             gpio.setup(g, gpio.OUT)
+#
+#     def makeTick(self, gpio_num):
+#         gpio.output(gpio_num, gpio.HIGH)
+#         gpio.output(gpio_num, gpio.LOW)
+#
+#     def shift(self, i, shift_data):
+#         for d in shift_data:
+#             gpio.output(self.DS[i], int(d))
+#             self.makeTick(self.SHCP[i])
+#         self.makeTick(self.STCP[i])
 
 class LEDMatrix:
     def __init__(self, num_layer=20):
