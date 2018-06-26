7106bac43d3ee3efe3e748c7c66cf6a767101f76
Jackson Chen <teacher144123@gmail.com>
2018-06-26 18:32:56 +0800

    add spi le

--- test/spi_led.py
+++ test/spi_led.py
@@ -1,7 +1,7 @@
-import spi
+import spidev
 
 
 if __name__ == '__main__':
-    register = spi.SPI(clk=22, cs=27, mosi=17, miso=None, verbose=True)
+    register = spi.SpiDev(clk=22, cs=27, mosi=17, miso=None, verbose=True)
 
     register.put(int("001000100111",2),12)
