7a0eec2575fb5b04d8d756319670ce93ff914361
Jackson Chen <teacher144123@gmail.com>
2018-06-26 21:33:56 +0800

    setting cleanUP

--- matrix_2p.py
+++ matrix_2p.py
@@ -18,9 +18,11 @@ class LEDMatrix:
         self.maps, self.graph = self.makeGraph(num_layer)
 
     def cleanUp(self):
-        # address = ['0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
-        # zero = ['00000000']
-        self.register.shift(0, '110000000000')
+        address = ['0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
+        zero = ['00000000']
+        for i in range(8):
+            self.register.shift(0, address[i] + zero)
+        # self.register.shift(0, '110000000000')
 
     def show8x8(self, graph_s, sec):
         address = ['0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000']
