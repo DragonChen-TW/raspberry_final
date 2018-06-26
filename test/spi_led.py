e494b472b67aecee092218fabc6d27d40d5592ca
Jackson Chen <teacher144123@gmail.com>
2018-06-26 18:55:28 +0800

    try setting

--- test/spi_led.py
+++ test/spi_led.py
@@ -5,16 +5,6 @@ import spi
 if __name__ == '__main__':
     register = spi.SPI(clk=22, cs=27, mosi=17, miso=None, verbose=True)
 
-    ### Disable code B decode mode on all digits
-    register.put(int("100100000000",2), 16)
-    register.put(int("001000100111",2),12)
-    time.sleep(1)
-
-    ### Set intensity low
-    register.put(int("101000000000",2), 16)
-    register.put(int("001000100111",2),12)
-    time.sleep(1)
-
     ### Enable all digits in scan-limit register
     register.put(int("101100000111",2), 16)
     register.put(int("001000100111",2),12)
