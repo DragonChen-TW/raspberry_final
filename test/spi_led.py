57ead1fd132bc78e248c609b3d39f0cf765a0ab2
Jackson Chen <teacher144123@gmail.com>
2018-06-26 19:05:51 +0800

    try setting

--- test/spi_led.py
+++ test/spi_led.py
@@ -5,11 +5,13 @@ import spi
 if __name__ == '__main__':
     register = spi.SPI(clk=22, cs=27, mosi=17, miso=None, verbose=True)
 
-    for i in range(8):
+    for i in range(16):
         register.put((i + 1) << 8, 12)
 
     ### Set the scan limit register and disable shutdown mode
     register.put(int("101100000111",2),12)
-    register.put(int("110000000001",2),12)
+    register.put(int("110000000001",2),12
+
+    register.put(int('010011100111', 2), 12)
 
     input("Press any key to shut down.")
