f24f69a2fd45421335116e95e0bedde5f1544b52
Jackson Chen <teacher144123@gmail.com>
2018-06-26 20:39:01 +0800

    cut file and add cleanup

--- matrix_2p.py
+++ matrix_2p.py
@@ -2,25 +2,7 @@ import RPi.GPIO as gpio
 import json, time, random
 from threading import Thread
 
-class Register:
-    def __init__(self, DS, SHCP, STCP):
-        self.DS = DS
-        self.SHCP = SHCP
-        self.STCP = STCP
-
-        gpio.setmode(gpio.BCM)
-        for g in self.DS + self.SHCP + self.STCP:
-            gpio.setup(g, gpio.OUT)
-
-    def makeTick(self, gpio_num):
-        gpio.output(gpio_num, gpio.HIGH)
-        gpio.output(gpio_num, gpio.LOW)
-
-    def shift(self, i, shift_data):
-        for d in shift_data:
-            gpio.output(self.DS[i], int(d))
-            self.makeTick(self.SHCP[i])
-        self.makeTick(self.STCP[i])
+from register import Register
 
 class LEDMatrix:
     def __init__(self, num_layer=20):
@@ -34,6 +16,10 @@ class LEDMatrix:
         self.max_layer = num_layer
 
         self.maps, self.graph = self.makeGraph(num_layer)
+
+    def cleanUp(self):
+        zeros = ['00000000'] * 8
+        self.show8x8(zeros)
 
     def show8x8(self, graph_s, sec):
         address = ['0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000']
---
+++ register.py
@@ -0,0 +1,19 @@
+class Register:
+    def __init__(self, DS, SHCP, STCP):
+        self.DS = DS
+        self.SHCP = SHCP
+        self.STCP = STCP
+
+        gpio.setmode(gpio.BCM)
+        for g in self.DS + self.SHCP + self.STCP:
+            gpio.setup(g, gpio.OUT)
+
+    def makeTick(self, gpio_num):
+        gpio.output(gpio_num, gpio.HIGH)
+        gpio.output(gpio_num, gpio.LOW)
+
+    def shift(self, i, shift_data):
+        for d in shift_data:
+            gpio.output(self.DS[i], int(d))
+            self.makeTick(self.SHCP[i])
+        self.makeTick(self.STCP[i])
